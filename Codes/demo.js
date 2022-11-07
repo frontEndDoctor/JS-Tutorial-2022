fe4d67a7dc120009d3a125ed6b54067a8c8c8640
Maybell Obadoni <wilsonmaybell@gmail.com>
2022-11-07 20:59:37 +0100

    Add files via upload

---
+++ Codes/demo.js
@@ -0,0 +1,145 @@
+//Arrow function
+//For in
+//for Each
+//Map
+// To-do List App
+
+// var example =(x) => {
+//   return x * 3;
+// }
+// x = 5;
+// console.log(example());
+
+// let a = 5;
+// let b = 10;
+// console.log (a+b);
+
+
+// cars.forEach(element => {
+//     carFunction();
+// });
+// function carFunction() {
+//     console.log(" This is a car");
+// }
+// check update on forEach method
+// let cars =['BMW',]
+// cars.forEach(carFunction);
+
+// function carFunction(item,index) {
+//     console.log("That is my car");
+// }
+// for (const key in object) {
+//     if (Object.hasOwnProperty.call(object, key)) {
+//         const element = object[key];
+        
+//     }
+// }
+
+// for (i in cars )[
+//     console.log(cars)
+// cars.forEach(element => {
+//     carFunction();
+// });
+// function carFunction() {
+//     console.log(" This is a car");
+// }
+
+// for (const car in cars) {
+//     if (Object.hasOwnProperty.call(object, key)) {
+//         const element = object[key];
+        
+//     }
+// }
+// let scores =[10,20,30,40]
+// const newScores = scores.map(addScores)
+// function addScores(score) {
+//     return score +5;
+// }
+// console.log (newScores)
+
+// // to-do List App
+// let tasks =[' Wash', 'Eat', 'Read']
+// const newTasks = tasks.map(taskFunction)
+// function taskFunction(add){
+//  return tasks.push['Sleep']
+// }
+
+// var submitForm =() =>{
+
+//     alert("Login Successful!!!");
+// }
+// submitForm();
+
+// document.getElementById("second").style = "background-color: blue";
+// document.getElementById("second").style = "color:white";
+// document.getElementsByClassName("list").style ="display: float-right";
+// document.getElementsByTagName("header").innerHTML = "Sign-in Page";
+
+
+
+// Steps in building a To-do list App
+//Features of our To-do List APP
+// input box for entering new task
+//  Update task list
+// Add/remove from task
+// let todo = prompt("enter a task")
+// let tasks =[ ]
+// var newTasks =() => {
+ // type in what you want to be done when the  onclick button is clicked
+//  if (todo!==" "){
+//      tasks+=todo;
+//      return todo;
+//  }
+
+// }
+
+
+// More examples on DOM
+// JS WEB API
+// JQUERY
+//JSON
+//BOOTSTRAP
+//REACTJS
+// var scores =[40,50,70,20,10,80]
+// const i = 's';
+// for (let s = 0; s < scores.length; scores+=2 ){
+//         console.log(scores)
+//         break;
+// }
+// document.getElementsByClassName("par").style = "background-color:red";
+
+//API (Application Programming Interface)
+
+//WEB API
+// Geolocation API
+const myLocation = document.getElementById("pos");
+function getLocation(){
+    try {
+        navigator.geolocation.getCurrentPosition(showLocation);
+    } catch (error) {
+        myLocation.innerHTML = "Location not found";
+    }
+}
+function showLocation(position) {
+    myLocation.innerHTML = "This is where I am currently" + position.coords.longitude;
+    
+}
+// API- Application Programming Interface
+// Form API
+// declaring a function caled submitForm to validate the email address
+
+function submitForm() {
+    const myEmail = document.getElementById('chk');
+    if (myEmail.checkValidity()) {
+        document.getElementsByClassName("ran").innerHTML= myEmail.validationMessage;
+        
+    }
+    else{
+        document.getElementsByClassName("ran").innerHTML= myEmail.validationMessage;;
+    }
+    
+}
+//History API
+//write a function using History API
+
+//Fetch API
